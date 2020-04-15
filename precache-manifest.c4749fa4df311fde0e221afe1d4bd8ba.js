2f5787eb53227b5aa2ca13ae3177a162f98e83ad
github_YusufKer <yus.ker1994@gmail.com>
2020-04-16 00:50:23 +0200

    Updates

---
+++ precache-manifest.c4749fa4df311fde0e221afe1d4bd8ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257de0f179fba059939836bc5c53e14b",
+    "url": "/weather_widget/index.html"
+  },
+  {
+    "revision": "feaa26c506e5d25e42f2",
+    "url": "/weather_widget/static/css/main.2bc0472d.chunk.css"
+  },
+  {
+    "revision": "7e16eb89a555c98271ee",
+    "url": "/weather_widget/static/js/2.c24b7a61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_widget/static/js/2.c24b7a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feaa26c506e5d25e42f2",
+    "url": "/weather_widget/static/js/main.aaf387bf.chunk.js"
+  },
+  {
+    "revision": "8b33a76cf9e0912bba57",
+    "url": "/weather_widget/static/js/runtime-main.6090b882.js"
+  }
+]);
